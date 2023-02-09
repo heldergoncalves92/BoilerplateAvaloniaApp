4bf49ca6a0721b75309ba2c6f922fdec14a1a10a
Hélder Gonçalves <helder.goncalves@outsystems.com>
2023-02-09 10:28:45 +0000

    Boirlerplate App

---
+++ CefGlue.Demo.Avalonia/Aggregator/AggregatorView.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+using Avalonia.VisualTree;
+
+namespace ServiceStudio.WebViewImplementation {
+    internal partial class AggregatorView : UserControl {
+        public AggregatorView() { }
+
+        public AggregatorView(TabHeaderInfo tabHeaderInfo) {
+            AvaloniaXamlLoader.Load(this);
+            var btn = this.FindControl<Button>("btn");
+            btn.Click += BtnOnClick;
+
+            var btnModal = this.FindControl<Button>("btn-modal");
+            btnModal.Click += BtnModalOnClick;
+            TabHeader = tabHeaderInfo;
+        }
+
+        private void BtnModalOnClick(object sender, RoutedEventArgs e)
+        {
+            var w = new Window { Width = 200, Height = 200 };
+            w.ShowDialog(this.GetVisualRoot() as Window);
+        }
+
+        private void BtnOnClick(object sender, RoutedEventArgs e)
+        {
+            var w = new Window { Width = 200, Height = 200 };
+            w.Show(this.GetVisualRoot() as Window);
+        }
+
+        public TabHeaderInfo TabHeader { get; }
+    }
+}
