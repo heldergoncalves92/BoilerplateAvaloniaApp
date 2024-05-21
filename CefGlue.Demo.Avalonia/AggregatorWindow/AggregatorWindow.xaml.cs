3b3973881e6bc05090c35a3a105e65859e5c4e91
Hélder Gonçalves <helder.goncalves@outsystems.com>
2024-05-21 16:25:19 +0100

    Support Native Menus

--- CefGlue.Demo.Avalonia/AggregatorWindow/AggregatorWindow.xaml.cs
+++ CefGlue.Demo.Avalonia/AggregatorWindow/AggregatorWindow.xaml.cs
@@ -118,5 +118,24 @@ namespace ServiceStudio.WebViewImplementation {
         private void OnDragEnter(object sender, Avalonia.Input.DragEventArgs e) {
             tabs.SelectedIndex = GetTabIndex(((TabItem)sender).Content as ITopLevelView);
         }
+
+        protected override void OnLoaded(RoutedEventArgs e) {
+            base.OnLoaded(e);
+            
+            var nativeMenu = NativeMenu.GetMenu(this);
+            var menuItem = new NativeMenuItem("AmazingNativeMenu");
+            var menu = new NativeMenu();
+            menu.NeedsUpdate += (_, __) => Console.WriteLine("Menu Needs To Update");
+            
+            var item = new NativeMenuItem();
+            item.Click += (_, __) => Console.WriteLine("OnMenuItemClick");
+            item.Header = "Delete Amazing Action";
+            item.IsEnabled = true;
+            item.Gesture = KeyGesture.Parse("Tab");
+            menu.Items.Add(item);
+            
+            menuItem.Menu = menu;
+            nativeMenu.Add(menuItem);
+        }
     }
 }
