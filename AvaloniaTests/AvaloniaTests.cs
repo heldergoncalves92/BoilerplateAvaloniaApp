d3529125c2ce7dc53f932cb03918ea818d63ec9f
Hélder Gonçalves <helder.goncalves@outsystems.com>
2024-09-17 17:43:43 +0100

    Add Project Test

---
+++ AvaloniaTests/AvaloniaTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace CefGlue.Demo.Avalonia.Tests;
+
+public class AvaloniaTests {
+    
+    [Test]
+    public void Test1() {
+        Console.WriteLine("RUM");
+    }
+    
+    [Test]
+    public void Test3() {
+        Console.WriteLine("RUM");
+    }
+}
